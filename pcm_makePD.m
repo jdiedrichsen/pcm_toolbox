125b995fe285c9580694e5abb02aac2f8a7d1693
Atsushi Yokoi <at.yokoi.work@gmail.com>
2016-10-21 14:19:19 +0900

    small change

--- pcm_makePD.m
+++ pcm_makePD.m
@@ -1,15 +1,20 @@
-function Gpd = pcm_makePD(G)
-% function Gpd = pcm_makePD(G)
+function Gpd = pcm_makePD(G,th)
+% function Gpd = pcm_makePD(G,th)
 % Enforces that G is semi-positive definite by removing possible negative
 % eigenvectors from the square matrix 
 % INPUT: 
 %       G:      estimated KxK second momement matrix 
+%       th:     threshold
 % OUTPUT:
 %       Gpd :   semi-positive definite version of G   
 
+if nargin<2
+    th = eps;
+end
+    
 % Make the G-estimate postive definite: 
 G = (G+G')/2;        % Symmetrize 
 [V,lam_G] = eig(full(G));
 dS    = diag(lam_G);
-idx   = dS>eps;
+idx   = dS>th;
 Gpd     = V(:,idx)*lam_G(idx,idx)*V(:,idx)'; 
