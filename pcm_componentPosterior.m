e06b6bf2a04da16f0b7e82eb7d1838f71524aa54
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2017-11-27 11:44:29 -0500

    componentPosterior in log space
    The component posterior is now being computed in log space, (hopefully)
    making it save to numerical overflow issues

--- pcm_componentPosterior.m
+++ pcm_componentPosterior.m
@@ -1,4 +1,4 @@
-function [postProp,logBayes]=pcm_componentPosterior(likelihood,compI,varargin); 
+function [postProb,logBayes]=pcm_componentPosterior(likelihood,compI,varargin); 
 % function p=pcm_componentPosterior(likelihood,compI); 
 % Calculates a posterior proability for the presence of each 
 % component from a family of models 
@@ -9,25 +9,42 @@ function [postProp,logBayes]=pcm_componentPosterior(likelihood,compI,varargin);
 %   compI:      (numModels x numComp) Set of indicator variables showing the presence / absence
 %               of the components for each fitted (pcm_constructModelFamily)
 % VARARGIN: 
-%  prior:       Prior probability of each model (assumed to be flat over
-%               models) 
-% OUTPUT:  
-%               
+%  prior:       Prior probability of each model
+%               - A numModelsx1 vector  
+%               - A scalar is interpreted as the base-probability for each component to be present  
+%               - If empty, it defaults to a flat prior over models 
+% OUTPUT:      
+%  postProb:    (numSubj x numComp) Posterior probability of the presence of all components for all subjects 
+%  logBayes:    (numSubj x numComp) Log Bayes factor of the models assuming
+%               presence of factor over the models asssuming absence 
+% 2017 by Joern Diedrichsen & Eva Berlot 
+
 prior = []; 
 pcm_vararginoptions(varargin,{'prior'}); 
 
 [numModels,numComp]=size(compI); 
 
 if (isempty(prior))
-    prior = ones(1,numModels)./numModels; 
+    logPrior = log(ones(1,numModels)./numModels);
+elseif isscalar(prior) 
+    logPrior = sum(compI.*log(prior)+(1-compI).*log(1-prior),2)'; 
 end; 
 
-nCondLogL=bsxfun(@minus,likelihood,mean(likelihood,2)); % Conditional log-probability up to a constant scaling factor 
-condProp  = exp(nCondLogL); 
-jointProp = bsxfun(@times,condProp,prior); 
+logJoint = bsxfun(@plus,likelihood,logPrior);
+logJoint = bsxfun(@minus,logJoint,max(logJoint,[],2))+50;   % Scale the log-joint, so it doesn't give errors with exp
 
 for i=1:numComp 
     indx = compI(:,i)==1; 
-    postProp(:,i)=sum(jointProp(:,indx),2)./sum(jointProp,2); % Posterior proability of component 
-    logBayes(:,i)=log(sum(jointProp(:,indx),2))-log(sum(jointProp(:,~indx),2)); % Log-Bayes factor in favor of component 
+   % check if any of components all set to 1s/0s
+   % calculate posterior probability of component
+   % calculate logBayes factor in favor of component 
+   if sum(indx)==length(indx) || sum(indx)==0
+       logPosterior(:,i)=logPrior(i);
+       logBayes(:,i)=NaN;
+   else
+       logPosterior(:,i)=log(sum(exp(logJoint(:,indx)),2))-log(sum(exp(logJoint),2));
+       logBayes(:,i)=log(sum(exp(logJoint(:,indx)),2))-log(sum(exp(logJoint(:,~indx)),2));
+   end
 end; 
+
+postProb = exp(logPosterior); 
