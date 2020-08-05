419b1b5b737c627b13e8ee5eade6efe86024f918
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2020-08-05 13:56:02 -0400

    Scale Prior for individual fit

--- recipe_finger/testing_script.m
+++ recipe_finger/testing_script.m
@@ -4,10 +4,19 @@ M{1}.numGparams = 1;
 M{1}.Gc         = Model(1).G;
 M{1}.name       = 'muscle';
 
-YY = Y{1}*Y{1}';
-n_channels = size(Y{1},2); 
-Z = indicatorMatrix('identity',condVec{1});
-X = indicatorMatrix('identity',partVec{1});
-theta = [-0.5;0.5;0.1]; 
+M{2}.type       = 'component';
+M{2}.numGparams = 1;
+M{2}.Gc         = Model(2).G;
+M{2}.name       = 'muscle';
 
-[lik,dL,dLL] = pcm_likelihoodIndivid(theta,YY,M{1},Z,[],n_channels,'fitScale',0,'runEffect',X);
+M{3}.type       = 'component';
+M{3}.numGparams = 2;
+M{3}.Gc(:,:,1)  = Model(1).G;
+M{3}.Gc(:,:,2)  = Model(2).G;
+M{3}.name       = 'muscle+nat';
+
+[Z,B,X,YY,Ss,N,P,G_hat,noise0,run0]=pcm_setUpFit(Y,partVec,condVec,'runEffect','fixed');
+theta0={zeros(15,1),zeros(15,1),zeros(16,1)};
+[T1,theta1]=pcm_fitModelGroup(Y,M{3},partVec,condVec,'fitScale',1,'runEffect','fixed');
+[T2,theta2]=pcm_fitModelGroupCrossval(Y,M{3},partVec,condVec,'fitScale',1,'runEffect','fixed','groupFit',theta1);
+keyboard; 
