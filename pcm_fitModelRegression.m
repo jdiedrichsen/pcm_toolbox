7ac705d365e273d5f81a9c32a7f3fa62bf30b7e1
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2020-11-20 16:54:35 -0500

    Likelihood functions

--- pcm_fitModelRegression.m
+++ pcm_fitModelRegression.m
@@ -2,16 +2,15 @@ function [theta_hat,INFO]=pcm_fitModelRegression(Z,Y,comp,X,varargin);
 % [theta_hat,G_pred,INFO]=pcm_fitModelRegression(Z,Y,comp,varargin);
 % Fits a Tikhonov-regression model to the data Y 
 
-runEffect       = 'fixed';
-isCheckDeriv    = 0;
+likeFcn         = 'auto';
 maxIteration    = [];      % MaxIteration - otherwise algorithm-specific defaults 
 likeThres       = [];    % if log-likelihood decreases less that this value, it stops (for pcm_NR)
 verbose         = 1;   % 1: Indicating the subject 2: detailed feedback
 S               = [];
-fitAlgorithm    = [];
+fitAlgorithm    = 'newton';
 theta0          = [];
-fitScale        = 0; 
-pcm_vararginoptions(varargin,{'runEffect','isCheckDeriv','maxIteration',...
+
+pcm_vararginoptions(varargin,{'','isCheckDeriv','maxIteration',...
     'verbose','S','fitAlgorithm','theta0','fitScale','likeThres'});
 
 % Set options for fitting algorithm 
@@ -25,5 +24,8 @@ end
 
 nParam = max(comp)+1;
 theta0 = zeros(nParam,1); 
+
+
+
 fcn = @(x) pcm_likelihoodRegression(x,Z,Y,comp,X,'S',S);
 [theta_hat,INFO.like,INFO.iter,INFO.regFinal,INFO.regH,INFO.thetaH]= pcm_NR(theta0,fcn,fitOPT); 
--- pcm_likelihoodRegression_YTY_ZTZ.m
+++ pcm_likelihoodRegression_YTY_ZTZ.m
@@ -1,15 +1,15 @@
-function [negLogLike,dnl,d2nl] = pcm_likelihoodRegression_YYT_ZZT(theta,Z,YY,P,comp,X,varargin);
-% function [negLogLike,dnl,d2nl] = pcm_likelihoodRegression_YYT_ZTZ(theta,Z,YY,P,comp,X,varargin);
+function [negLogLike,dnl,d2nl] = pcm_likelihoodRegression_YTY_ZTZ(theta,Z,Y,comp,X,varargin);
+% function [negLogLike,dnl,d2nl] = pcm_likelihoodRegression_YTY_ZTZ(theta,Z,Y,comp,X,varargin);
 % Returns negative log likelihood for the model, and the derivatives in
 % respect to the model parameters. This is version of pcm_likelihood, which
 % is designed specifically for determining the ridge coefficient for groups
-% of features. 
+% of features.
 % This version of the likelihood function is fastest if P>N and Q > N
 %
 % INPUT:
 %      theta:   Vector of (log-)model parameters: These include model
 %               parameters, noise parameter, and (optional) run parameter
-%      Y:       NxP Matrix of data 
+%      Y:       NxP Matrix of data
 %      Z:       NxK Design matrix - relating the trials (N) to the random effects (K)
 %      comp:    1xK vector of groups that determine the groups of features
 %               in the design matrix that correspond to a theta coefficient
@@ -35,24 +35,28 @@ function [negLogLike,dnl,d2nl] = pcm_likelihoodRegression_YYT_ZZT(theta,Z,YY,P,c
 [N,P] = size(Y);
 K = size(Z,2);
 S = [];
-nComp = max(comp); 
-nParam = length(theta); 
+nComp = max(comp);
+nParam = length(theta);
 pcm_vararginoptions(varargin,{'S'});
 
-
-% Split the paraemters in noise and ridge parameter 
-modelParam = theta(1:nComp); 
+% Split the paraemters in noise and ridge parameter
+modelParam = theta(1:nComp);
 noiseParam = theta(nComp+1:end);
 
-% Find the inverse of V 
+% Find the inverse of V
 % Apply the matrix inversion lemma. The following statement is the same as
 % V   = (Z*G*Z' + S.S*exp(theta(H))); % As S is not identity, matrix inversion lemma does not have big advantage here (ay)?
 % iV  = pinv(V);
-iG = 1./exp(modelParam(comp)); 
+G  = exp(modelParam(comp));
+iG = 1./ G;
 if (isempty(S))
     iV    = (eye(N)-Z/(diag(iG)*exp(noiseParam)+Z'*Z)*Z')./exp(noiseParam); % Matrix inversion lemma
+    Zw = bsxfun(@times,Z,sqrt(G)');
+    lam = eig(Zw' * Zw);
+    ldet  = sum(log(lam+exp(noiseParam))) + (N - K)*noiseParam;
 else
     iV    = (S.invS-S.invS*Z/(diag(diag(iG)*exp(noiseParam)+Z'*S.invS*Z)*Z'*S.invS))./exp(noiseParam); % Matrix inversion lemma
+    ldet  = -2* sum(log(diag(chol(iV))));        % Safe computation of the log determinant (V) Thanks to code from D. lu
 end;
 iV  = real(iV); % sometimes iV gets complex
 
@@ -65,43 +69,48 @@ else
 end;
 
 % Computation of (restricted) likelihood
-B     = Y * Y' * iVr;
-ldet  = -2* sum(log(diag(chol(iV))));        % Safe computation of the log determinant (V) Thanks to code from D. lu
-l     = -P/2*(ldet)-0.5*trace(B);
+YiVr     = Y' * iVr;
+l     = -P/2*(ldet)-0.5*traceAB(YiVr,Y);
 if (~isempty(X)) % Correct for ReML estimates
     l = l - P*sum(log(diag(chol(X'*iV*X))));  % - P/2 log(det(X'V^-1*X));
-end;
+end
 negLogLike = -l; % Invert sign
 
 % Calculate the first derivative
 if (nargout>1)
     % Get the derivatives for all the parameters
-    for i = 1:length(modelParam)
-        iVdV{i} = iVr*Z(:,comp==i)*Z(:,comp==i)'*exp(modelParam(i));
-        dLdtheta(i,1) = -P/2*trace(iVdV{i})+1/2*traceABtrans(iVdV{i},B);
+    for i = 1:nParam
+        if i<=nComp
+            iVZ{i} = iVr*Z(:,comp==i);
+            A = Y' * iVZ{i}; 
+            dLdtheta(i,1) = -exp(modelParam(i))/2*(P*traceABtrans(iVZ{i},Z(:,comp==i)) - traceABtrans(A,A));
+        else
+            if (isempty(S))
+                iVZ{i} = iVr*exp(noiseParam);
+            else
+                iVZ{i} = iVr*S*exp(noiseParam);
+            end
+            dLdtheta(i,1) = -P/2*trace(iVZ{i})+1/2*traceABtrans(Y' * iVZ{i},YiVr);
+        end
+        
+        % invert sign
+        dnl   = -dLdtheta;
     end
-    
-    % Get the derivatives for the Noise parameters
-    i = length(modelParam)+1;  % Which number parameter is it?
-    if (isempty(S))
-        dVdtheta{i}          = eye(N)*exp(noiseParam);
-    else
-        dVdtheta{i}          = S.S*exp(noiseParam);
-    end;
-    iVdV{i}     = iVr*dVdtheta{i};
-    dLdtheta(i,1) = -P/2*trace(iVdV{i})+1/2*traceABtrans(iVdV{i},B);
-    
-    % invert sign
-    dnl   = -dLdtheta;
-end;
+end
 
 % Calculate expected second derivative?
 if (nargout>2)
     for i=1:nParam
         for j=i:nParam
-            d2nl(i,j)=-P/2*traceABtrans(iVdV{i},iVdV{j});
+            if j<=nComp
+                d2nl(i,j)=-P/2*traceAB(Z(:,comp==j)'*iVZ{i},Z(:,comp==i)' * iVZ{j}) * exp(modelParam(i))*exp(modelParam(j));
+            elseif i<=nComp
+                d2nl(i,j)=-P/2*traceAB(iVZ{i},Z(:,comp==i)' * iVZ{j}) * exp(modelParam(i));
+            else
+                d2nl(i,j)=-P/2*traceAB(iVZ{i},iVZ{j});
+            end
             d2nl(j,i)=d2nl(i,j);
-        end;
-    end;
+        end
+    end
     d2nl=-d2nl;
 end;
