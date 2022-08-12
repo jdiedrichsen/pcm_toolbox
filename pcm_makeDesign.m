7ffa49ae5526a05e889c74a697aa0cd24d800fa1
Jörn Diedrichsen <joern.diedrichsen@googlemail.com>
2022-08-12 15:33:29 -0400

    Variable in pcm_makeDesign

--- pcm_makeDesign.m
+++ pcm_makeDesign.m
@@ -1,12 +1,14 @@
 function [condVec,partVec] = pcm_makeDesign(varargin);
 % function [condVec,partVec] = pcm_makeDesign(varargin);
 % VARARGIN: 
+%   'numCond',number of partitions  (default 8)
 %   'numPart',number of partitions  (default 8)
 % OUTPUT: 
 %    condVec:    Vector of conditions for RSA-style model 
 %    partVec:    Vector indicating the independent partitions 
 numPart = 8; 
-numCond = 5; 
+numCond = 5;
+vararginoptions(varargin,{'numPart','numCond'})
 N          = numPart*numCond;          % Number of trials
 partVec    = kron([1:numPart]',ones(numCond,1));            % Partitions
 condVec    = kron(ones(numPart,1),[1:numCond]');            % Conditions
